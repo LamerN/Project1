224fbc13799168ccc1c54a944320bac73a113fff
LamerN <90185479+LamerN@users.noreply.github.com>
2021-09-07 00:04:37 +0300

    mnbmn

---
+++ src/Lesson2/Main.java
@@ -0,0 +1,12 @@
+package Lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        long l1 = 10000000000L;
+
+        System.out.print("l1 =");
+        System.out.println(l1);
+
+    }
+}
---
+++ src/com/Lesson1/Main.java
@@ -0,0 +1,13 @@
+package com.Lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world!");
+        long l1 = 10000000000L;
+
+        System.out.print("l1 = ");
+        System.out.println(l1);
+    }
+}
