926c0462252d972d56cc0d7be3d2b54495e14de6
LamerN <90185479+LamerN@users.noreply.github.com>
2021-09-07 23:27:08 +0300

    lesson2 homework

---
+++ src/Homework/Lesson2.java
@@ -0,0 +1,9 @@
+package Homework;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        System.out.println(1);
+
+    }
+
+}
--- src/com/Lesson1/Lesson1.java
+++ src/com/Lesson1/Lesson1.java
@@ -1,6 +1,6 @@
 package com.Lesson1;
 
-public class Main {
+public class Lesson1 {
 
     public static void main(String[] args) {
 	// write your code here
