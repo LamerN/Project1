8b04f506f22d88bc0515d8f9d3068a893467eded
LamerN <90185479+LamerN@users.noreply.github.com>
2021-09-11 20:25:31 +0300

    lesson3

--- src/Lesson3/Test3.java
+++ src/Lesson3/Test3.java
@@ -3,5 +3,11 @@ package Lesson3;
 public class Test3 {
     public static void main (String[] args) {
         int a=1, b=2, c=4;
+        b++;
+        boolean x=true;
+        boolean y=true;
+        boolean z=false;
+
+        System.out.println(b);
     }
 }
