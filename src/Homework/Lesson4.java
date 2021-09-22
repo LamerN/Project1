3b8b04eef88180d6e21a68e1720a4ab1c95cca60
LamerN <90185479+LamerN@users.noreply.github.com>
2021-09-23 01:11:17 +0300

    lesson3

---
+++ src/Homework/Lesson4.java
@@ -0,0 +1,16 @@
+package Homework;
+
+class Student{
+    int ticket_number;
+    String name;
+    String lastName;
+    int yearOfStady;
+    int markMathmatic;
+    int markEconomy;
+    int markLanguage;
+
+
+        }
+
+public class Lesson4 {
+}
