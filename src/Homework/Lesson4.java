6289ea98b747302c2952274897a3c173aef2ec7d
LamerN <90185479+LamerN@users.noreply.github.com>
2021-09-25 01:23:44 +0300

    lesson4 Homework

--- src/Homework/Lesson4.java
+++ src/Homework/Lesson4.java
@@ -9,7 +9,23 @@ class Student{
     int markEconomy;
     int markLanguage;
 
+class StudentTest {
 
+    public static void main(String[] args) {
+        Student Andy = new Student();
+        Andy.ticket_number = 1;
+        Andy.name = "Andy";
+        Andy.lastName = "Star";
+        Andy.yearOfStady = 2;
+        Andy.markMathmatic = 4;
+        Andy.markEconomy = 5;
+        Andy.markLanguage = 2;
+
+        int averageMarkAndy = (Andy.markMathmatic+ Andy.markEconomy + Andy.markLanguage)/3;
+        System.out.println(Andy.name+" "+Andy.lastName+" - average mark = "+averageMarkAndy);
+
+    }
+}
         }
 
 public class Lesson4 {
