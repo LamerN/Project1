5f1c60b373e842e4f0e08cf93194722b779ef73f
LamerN <90185479+LamerN@users.noreply.github.com>
2021-11-14 16:21:58 +0300

    lesson12 Homework

---
+++ src/Homework/Lesson14/Chasi.java
@@ -0,0 +1,23 @@
+package Homework.Lesson14;
+
+public class Chasi {
+    public static void time() {
+        outer: for (int hour = 0; hour <= 6; hour++) {
+            outer2: for (int minute = 0; minute<=60;minute++){
+                if ((hour>1)&&(minute%10==0)){
+                    break outer;
+                }
+                for (int second = 0; second<=60; second++){
+                    if (second*hour>minute){
+                        continue outer2;
+                    }
+                    System.out.println(hour+":"+minute+":"+second);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Chasi.time();
+    }
+}
