98e2f21fe8f2a711e275e91d2115199f82cdf18e
LamerN <90185479+LamerN@users.noreply.github.com>
2021-11-29 20:13:56 +0300

    lesson17 Homework

--- src/Homework/Lesson17/Test.java
+++ src/Homework/Lesson17/Test.java
@@ -2,15 +2,21 @@ package Homework.Lesson17;
 
 public class Test {
     public static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2){
-        if (sb1.equals(sb2)) {
-            return true;
+        if (sb1.length()==sb2.length()) {
+            for (int i = 0; i<sb1.length(); i++){
+                if (sb1.charAt(i)==sb2.charAt(i)){
+                    continue;
+                }
+                else return false;
+            }
+        return true;
         }
         else return false;
     }
 
     public static void main(String[] args) {
         StringBuilder sb1 = new StringBuilder("123");
-        StringBuilder sb2 = sb1;
+        StringBuilder sb2 = new StringBuilder("1238");
         System.out.println(ravenstvo(sb1,sb2));
 
     }
