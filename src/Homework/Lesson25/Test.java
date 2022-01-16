dbc182c39c13101d44b900d38c32972c7576582e
LamerN <90185479+LamerN@users.noreply.github.com>
2022-01-17 00:59:05 +0300

    lesson25 Homework task done

--- src/Homework/Lesson25/Test.java
+++ src/Homework/Lesson25/Test.java
@@ -1,24 +1,66 @@
-package Homework.Lesson24;
+package Homework.Lesson25;
 
 public class Test {
     public static void main(String[] args) {
-        Mechenosec mech = new Mechenosec("Mech");
-        System.out.println(mech.name);
-        mech.eat();
-        mech.swim();
-        mech.sleep();
-        Speakable ping = new Pingvin("Ping");
-        ping.speak();
-        Animal lion = new Lev("Lion");
-        System.out.println(lion.name);
-        lion.sleep();
-        lion.eat();
-        Mammal lion2 = new Lev("Lion2");
-        System.out.println(lion2.name);
-        lion2.run();
-        lion2.sleep();
-        lion2.eat();
-        lion2.speak();
+        Animal a1 = new Mechenosec("a1");
+        Animal a2 = new Lev("a2");
+        Animal a3 = new Pingvin("a3");
+        Fish f1 = new Mechenosec("f1");
+        Bird b1 = new Pingvin("b1");
+        Mammal m1 = new Lev("m1");
+        Mechenosec me1 = new Mechenosec("me1");
+        Pingvin p1 = new Pingvin("p1");
+        Lev l1 = new Lev("l1");
+        Speakable s1 = new Pingvin("s1");
+        Speakable s2 = new Lev("s2");
+
+        Speakable[] speak = new Speakable[]{s1, s2};
+        Animal[] anim = new Animal[]{a1, a2, a3, f1, b1, m1, me1, p1, l1};
+        for(Speakable s:speak){
+            if(s instanceof Mechenosec){
+                System.out.println(((Mechenosec) s).name);
+                ((Mechenosec)s).eat();
+                ((Mechenosec) s).sleep();
+                ((Mechenosec) s).swim();
+                s.speak();
+            }
+            if(s instanceof Pingvin){
+                System.out.println(((Pingvin) s).name);
+                ((Pingvin) s).eat();
+                ((Pingvin) s).sleep();
+                ((Pingvin) s).fly();
+                s.speak();
+            }
+            if (s instanceof Lev){
+                System.out.println(((Lev) s).name);
+                ((Lev) s).eat();
+                ((Lev) s).sleep();
+                ((Lev) s).run();
+                s.speak();
+            }
+        }
+        for (Animal a:anim){
+            if(a instanceof Mechenosec){
+                System.out.println(((Mechenosec) a).name);
+                ((Mechenosec)a).eat();
+                ((Mechenosec)a).sleep();
+                ((Mechenosec)a).swim();
+            }
+            if(a instanceof Pingvin){
+                System.out.println(((Pingvin) a).name);
+                ((Pingvin) a).eat();
+                ((Pingvin) a).sleep();
+                ((Pingvin) a).fly();
+                ((Pingvin) a).speak();
+            }
+            if (a instanceof Lev){
+                System.out.println(((Lev) a).name);
+                ((Lev) a).eat();
+                ((Lev) a).sleep();
+                ((Lev) a).run();
+                ((Lev)a).speak();
+            }
+        }
     }
 
 }
@@ -115,7 +157,6 @@ class Lev extends Mammal{
     void sleep() {
         System.out.println("Bolshuyu chast dnya lev spit!");
     }
-
     @Override
     void run() {
         System.out.println("Lev - eto ne samaya bistraya koshka!");
